9bbfbcf4ddb9be5ef15aac8aa82a19aefda67f69
James Kip <jameskip@me.com>
2017-12-15 19:56:21 -0600

    add: test

--- Chapter 2/questions.test.js
+++ Chapter 2/questions.test.js
@@ -1,6 +1,6 @@
 const { removeDups } = require('./questions.js');
 
-// 2.1 TODOS
+// 2.1 TODO
 test('should remove duplicates from unsorted list', () => {
   expect(removeDups(true)).toBe(true);
 });
